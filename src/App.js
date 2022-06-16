15298fad1bc4825be5a10a911d78d3ff2ee00e4c
MeghanaDevaliya <meghanadevaliya@gmail.com>
2022-06-16 16:16:53 +0000

    todo list project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,174 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+
+// component to complete , edit, delete list item in list,
+//  which is being passed to main component 
+
+const Todo = ({
+  todo, index, completeTodo, removeTodo, editTodo
+}) => {
+  const [isEditing, setIsEditing] = React.useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="todo"
+          >
+
+      {!isEditing ?
+      <div style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }} > {todo.text}</div>
+       : (
+        <InputForm
+          editTodo={editTodo}
+          defaultValue={todo.text}
+          index={index}
+          setIsEditing={setIsEditing}
+          isEditing={isEditing} 
+          style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}
+        />
+      )}
+
+      <div className="buttonContainer">
+        <button
+          className="completeBtn"
+          onClick={() => completeTodo(index)}
         >
-          Learn React
-        </a>
-      </header>
+          Complete
+        </button>
+        <button
+          data-testid=" close"
+          className="crossBtn"
+          onClick={() => removeTodo(index)}
+        >
+          Delete
+        </button>
+        <button
+          className="editBtn"
+          onClick={() => setIsEditing(!isEditing)}
+        >
+          Edit
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+//input field to edit the item in list//
+
+const InputForm = ({
+  defaultValue, editTodo, index, setIsEditing, isEditing
+}) => {
+  const [value, setValue] = React.useState(defaultValue);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    editTodo(index, value);
+    setIsEditing(!isEditing);
+  };
+  return (
+    <>
+      <input
+        data-testid="inputField"
+        type="text"
+        className="inputForm"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="saveButton" onClick={(e) => handleSubmit(e)}> Save</button>
+
+    </>
+  );
+};
+//add new todo to list//
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  };
+
+  return (
+    <>
+      <input
+        data-testid="inputField"
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="addTask"onClick={(e) => handleSubmit(e)}> Add Task</button>
+
+    </>
+  );
+};
+
+const App = () => {
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'Buy Bread',
+      isCompleted: false
+    },
+    {
+      text: 'Buy Milk',
+      isCompleted: false
+    },
+    {
+      text: 'Buy vegetables',
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [{ text }, ...todos];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const editTodo = (indexv, updtedTask) => {
+    const updatedTodos = todos.map((todo, index) => {
+      if (index === indexv) {
+        return { ...todo, text: updtedTask };
+      }
+      return todo;
+    });
+    console.log(updatedTodos);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="logo"data-testid="icon" />
+      <div className="todoList">
+
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+            editTodo={editTodo}
+
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,132 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import TodoList from '../src/App';
+// install fetch mock library  by using the command npm install --save-dev jest-fetch-mock
+import fetchMock from 'jest-fetch-mock';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+const removeTodo = jest.fn();
+const completeTodo = jest.fn();
+const editTodo = jest.fn();
+const mockedFunction = jest.fn();
+
+
+test('render test', async () => {
+    render(<TodoList />);
+    expect(screen.getByText('Buy Bread')).toBeTruthy();
+    expect(screen.getByText('Buy Milk')).toBeTruthy();
+    expect(screen.getByText('Buy vegetables')).toBeTruthy();
+    expect(screen.getAllByText('Complete')).toBeTruthy();
+    expect(screen.getAllByText('Edit')).toBeTruthy();
+    expect(screen.getAllByText('Add Task')).toBeTruthy();
+   
+});
+
+test('logo to be present', async () => {
+    render(<TodoList />);
+    expect(screen.getAllByTestId('icon')).toBeTruthy();
+});
+
+test('render input', () => {
+    render(<TodoList />);
+    const inputEl = screen.getAllByTestId('inputField');
+    expect(inputEl).toBeTruthy();
+});
+
+test('add task to the list from input box', () => {
+    render(<TodoList />);
+    const inputEl = screen.getAllByTestId('inputField');
+    expect(inputEl).toBeTruthy();
+});
+
+test('input value changes', async () => {
+    const { getByTestId } = render(
+        <TodoList />
+    );
+    const input = getByTestId('inputField');
+    fireEvent.change(input, { target: { value: 'ABCD' } });
+    expect(input.value).toBe('ABCD');
+});
+test('should add todo to the list', async () => {
+    render(
+        <TodoList />
+    );
+
+    const startTodos = [
+        {
+            text: 'Buy Bread',
+            isCompleted: false
+        },
+        {
+            text: 'Buy Milk',
+            isCompleted: false
+        },
+        {
+            text: 'Buy vegetables',
+            isCompleted: false
+        }
+    ];
+    const input = screen.getByTestId('inputField');
+    fireEvent.change(input, { target: { value: 'ABCD' } });
+    expect(input.value).toBe('ABCD');
+    fireEvent.click(screen.getByText('Add Task'));
+    fetchMock.mockResponse(JSON.stringify(startTodos));
+    const arrrayList = startTodos;
+    expect(arrrayList).toHaveLength(3);
+});
+
+test('should delete item in the list', async () => {
+    const item = [{ text: 'Buy Milk', isCompleted: false }];
+    const itemIndex = 3;
+    fetchMock.mockResponse(JSON.stringify(item));
+
+    render(
+        <TodoList
+            index={itemIndex}
+            todo={mockedFunction}
+            removeTodo={removeTodo}
+            editTodo={editTodo}
+            completeTodo={completeTodo}
+
+        />
+    );
+
+    const button = screen.getAllByText('Delete');
+    fireEvent.click(button[0]);
+    expect(mockedFunction).not.toBe(itemIndex);
+});
+test('edit button click', async () => {
+    const item = [{ text: 'Buy Milk', isCompleted: false }];
+    const itemIndex = 3;
+    render(
+        <TodoList
+            index={itemIndex}
+            todo={item}
+            removeTodo={removeTodo}
+            editTodo={editTodo}
+            completeTodo={completeTodo}
+        />
+    );
+
+    const button = screen.getAllByText('Edit');
+    fireEvent.click(button[0]);
+    expect(mockedFunction).not.toBe(itemIndex);
+});
+test('mark as complete', async () => {
+    const item = { text: 'Buy Milk', isCompleted: false };
+    const itemIndex = 3;
+    render(
+        <TodoList
+            index={itemIndex}
+            todo={item}
+            removeTodo={removeTodo}
+            editTodo={editTodo}
+            completeTodo={completeTodo}
+
+        />
+    );
+
+    const button = screen.getAllByText('Complete');
+    screen.debug();
+    fireEvent.click(button[0]);
+    expect(mockedFunction).not.toBe(itemIndex);
 });
